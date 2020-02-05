43d4d35cb35108aa84524a8e32ab8f8f86e4bc84
Phuc Tran Truong <ptruong@outlook.de>
2020-02-05 13:43:25 +0100

    Add SD4M relation types and role labels

--- eventx/__init__.py
+++ eventx/__init__.py
@@ -1,5 +1,12 @@
 NEGATIVE_TRIGGER_LABEL = 'O'
 NEGATIVE_ARGUMENT_LABEL = 'no_arg'
+SD4M_RELATION_TYPES = ['Accident', 'CanceledRoute', 'CanceledStop', 'Delay',
+                       'Obstruction', 'RailReplacementService', 'TrafficJam',
+                       NEGATIVE_TRIGGER_LABEL]
+ROLE_LABELS = ['location', 'delay', 'direction',
+               'start_loc', 'end_loc',
+               'start_date', 'end_date', 'cause',
+               'jam_length', 'route', NEGATIVE_ARGUMENT_LABEL]
 from eventx.dataset_readers import *
 from eventx.models import *
 from eventx.util import *
